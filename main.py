d1c4f5190c6f11cb12dfecb3d19bbf0af167b091
Jason Gallagher <jasong13@gmail.com>
2022-11-14 15:27:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Marvel Movie Character Creator")
+print("--")
+
+likes_to_hang = input("Do you like 'hanging around'? ")
+
+if likes_to_hang.lower() == 'yes':
+  print("Hmm, you must be spiderman!")
+  print()
+else:
+  print("Then you're not spider-man")
+  print()
+  
+gravelly_voice = input("Do you have a 'gravelly' voice? ")
+
+if gravelly_voice.lower() == 'yes':
+  print("You must be Korg!")
+  print()
+else:
+  print("You must not be Korg")
+  print()
+
+feeling_marvelous = input("Do you often feel 'Marvelous'? ")
+
+if feeling_marvelous.lower() == 'yes':
+  print("Aha! You're Captain Marvel! Hi!")
+  print()
+else:
+  print("Hmm, idk who you are")
+  print()
